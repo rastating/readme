7575d9f1110e2d33323d4d8ac26eaeafc27059a3
Rob <robert.carr@owasp.org>
2019-05-17 20:01:18 +0100

    Add adminer, reminder and info scripts + MySQL driver configuration

---
+++ roles/web/files/reminder.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <body>
+        <p>
+            Julian! Don&apos;t forget we need to harden the security on the web server. In case you have forgotten your access details, I&apos;ve put them in a txt file for you. It's in that place where I put that thing that time.
+        </p>
+
+        <img src="that-place-where-i-put-that-thing-that-time/565b0f4691fe5.jpg" />
+
+        <p>
+            Also, can you fix this search box? Sometimes it chucks errors depending on what I enter...
+        </p>
+
+        <?php
+            if ($_POST['username']) {
+                echo '<strong>';
+
+                if (strpos($_POST['username'], "'") !== false) {
+                    echo "ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '".htmlentities($_POST['username'], ENT_QUOTES)."' at line 1";
+                } else {
+                    echo 'No users with that name found';
+                }
+
+                echo '</strong>';
+                echo '<br />';
+            }
+        ?>
+
+        <form action="reminder.php" method="POST">
+            <input type="text" name="username" placeholder="Username">
+            <input type="submit" value="Lookup User">
+        </form>
+    </body>
+</html>
